5278edbf2b2b73b2466547fa6954257b9db32da5
Lukasz Rakowiecki <lukasz.er17@gmail.com>
2020-11-24 12:41:25 +0100

    Modify class TestApi -> ImageUploader

---
+++ src/main/java/pl/rakowiecki/springbootimageuploader/ImageUploader.java
@@ -0,0 +1,34 @@
+package pl.rakowiecki.springbootimageuploader;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class ImageUploader {
+
+    private final Cloudinary cloudinary;
+
+    public ImageUploader() {
+        cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "deulijail",
+                "api_key", "126681229686252",
+                "api_secret", "IIpcP6sWOe-RueFYiKg3UpZjGz0"));
+    }
+
+    public String uploadFile(String path) {
+        File file = new File(path);
+        Map uploadResult = null;
+        try {
+            uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO
+        }
+        return "";
+    }
+}
